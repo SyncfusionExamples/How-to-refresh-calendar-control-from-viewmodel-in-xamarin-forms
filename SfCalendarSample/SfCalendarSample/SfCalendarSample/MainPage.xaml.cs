49612fbb038920995d845405a5d3d41310b834ad
ROOT\vigneshkumar.ramasam <vigneshkumar.ramasamy@syncfusion.com>
2018-11-14 14:26:41 +0530

    calendar refresh sample committed.

---
+++ SfCalendarSample/SfCalendarSample/SfCalendarSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfCalendarSample
+{
+	public partial class MainPage : ContentPage
+	{  
+        public MainPage()
+		{
+			InitializeComponent();
+        }
+
+    }
+}
