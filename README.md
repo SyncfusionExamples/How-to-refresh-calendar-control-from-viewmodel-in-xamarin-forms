4cdb78571d21b1b94c6c91299fb86febc3ba779b
syncfusion <muniappan.subramanian@syncfusion.com>
2019-04-05 17:28:21 +0530

    Sample has been updated.

---
+++ README.md
@@ -0,0 +1,12 @@
+# How to refresh calendar control from view model in Xamarin.Forms?
+
+This example demonstrates to refresh calendar from view model in Xamarin.Forms.
+
+## <a name="requirements-to-run-the-demo"></a>Requirements to run the demo ##
+
+* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/).
+* Xamarin add-ons for Visual Studio (available via the Visual Studio installer).
+
+## <a name="troubleshooting"></a>Troubleshooting ##
+### Path too long exception
+If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
---
+++ RefreshCalendar/RefreshCalendar/RefreshCalendar/ViewModel/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Syncfusion.SfCalendar.XForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RefreshCalendar
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ICommand _refresh;
+
+        public ICommand Refresh
+        {
+            get
+            {
+                return _refresh;
+            }
+            set
+            {
+                _refresh = value;
+                RaisePropertyChanged("Refresh");
+            }
+        }
+
+        public ViewModel()
+        {
+            Refresh = new Command(ExecuteRefresh);
+        }
+
+        // Refresh Calendar from ViewModel
+        private void ExecuteRefresh(object obj)
+        {
+            if (obj is SfCalendar)
+            {
+                (obj as SfCalendar).Refresh();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
